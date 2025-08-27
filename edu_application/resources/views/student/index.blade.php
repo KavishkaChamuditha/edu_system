d12baafe815bccccfbb14ee46b65731a581368d7
kavishka chamuditha <kavishkachamuditha562@gmail.com>
2025-08-27 16:39:11 +0530

    Implement main layout and student index view with header, footer, and JS includes

---
+++ edu_application/resources/views/student/index.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.main")
+@include("partials.header")
+@section('title', "student creation")
+@section('content')
+
+    <h1>Hii</h1>
+
+ @include("partials.footer")
+ @include("partials.js_filles")
