6a2d104b722e487b33bec57c69e2ac9146e90257
kavishka chamuditha <kavishkachamuditha562@gmail.com>
2025-08-27 16:08:21 +0530

    first commit

---
+++ edu_application/database/migrations/2025_08_27_100606_create_classes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('grade', 20);
+            $table->string('subject', 100);
+            $table->string('teacher', 100);
+            $table->date('start_date');
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
